f7175133eb40a0d1ef60c48eee35669146a8e2fc
Create Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2019-07-27 11:07:03 +0200

    Add files via upload

--- bootstrap.py
+++ bootstrap.py
@@ -12,22 +12,34 @@ import sys
 
 _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
+print("")
+print("")
+print("HELLO THERE")
 def install_packages():
     """Install all packages listed in requirements.txt."""
     system_option = ''
     try:
         import lsb_release
+        print("lsb_release.get_distro_information()['ID'] = " + str(lsb_release.get_distro_information()['ID']))
         if lsb_release.get_distro_information()['ID'] in ['Raspbian',
                                                           'Debian']:
             system_option = '--system'
     except ImportError:
+        print("unable to determine your linux distribution. Will try to continue.")
         pass
 
-    cmd = (
-        '{} -m pip install {} --install-option="--prefix=" -t lib '
-        '-r requirements.txt'.format(sys.executable, system_option))
+    #cmd = (
+    #    '{} -m pip install {} --install-option="--prefix=" -t lib '
+    #    '-r requirements.txt'.format(sys.executable, system_option))
 
+    
+    cmd = 'pip3 install -r requirements.txt -t lib --no-binary fuzzywuzzy --prefix ""'
+    #cmd = (
+    #    '{} -m pip install --install-option="--prefix=" -t lib '
+    #    '-r requirements.txt'.format(sys.executable))
+    
+    
+    print("Command that will install the dependencies: " + str(cmd))
     try:
         subprocess.check_call(cmd,
                               stderr=subprocess.STDOUT,
@@ -35,18 +47,21 @@ def install_packages():
                               cwd=_BASE_DIR)
         return True
     except subprocess.CalledProcessError as e:
-        print(e)
+        print("Unable to install dependencies: " + str(e))
         return False
 
 
 try:
+    print("_BASE_DIR = " + str(_BASE_DIR))
     sys.path.append(os.path.join(_BASE_DIR, 'lib'))
 
     #from hermes_python.hermes import Hermes # noqa: F401
     #from hermes_python.ontology import * # noqa: F401
     import hermes_python  # noqa: F401
-    #import fuzzywuzzy
-except ImportError:
+    import fuzzywuzzy  # noqa: F401
+    import pyalsaaudio  # noqa: F401
+except ImportError as ex:
+    print("Import error: " + str(ex))
     # If installation failed, exit with 100 to tell the gateway not to restart
     # this process.
     if not install_packages():
--- package.sh
+++ package.sh
@@ -7,10 +7,17 @@ version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
 # Clean up from previous releases
 rm -rf *.tgz package
 rm -f SHA256SUMS
+rm -rf lib
 
 # Put package together
 mkdir package
-cp -r pkg LICENSE package.json *.py requirements.txt setup.cfg package/
+mkdir lib
+
+# Pull down Python dependencies
+pip3 install -r requirements.txt -t lib --no-binary fuzzywuzzy --prefix ""
+
+
+cp -r pkg lib deps LICENSE package.json *.py requirements.txt setup.cfg package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
 
