e83e1397b89f85bdc3c2eef9281a61ae7a017895
flatsiedatsie <unfold@gmail.com>
2023-01-04 17:19:03 +0100

    Delete speak.sh

--- snips/speak.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Current dir is called $DIR"
-echo "$1" | ${DIR}/nanotts -l ${DIR}/lang --speed 0.9 --pitch 1.2 --volume 1 -w -o response.wav 
-aplay response.wav
-#echo "Done speaking (shell)"
-exit 0
