ae2188c752e8eaab44fd65e3208b6f8f13d31a15
flatsiedatsie <unfold@gmail.com>
2024-02-16 17:51:46 +0100

    Add files via upload

--- pkg/util.py
+++ pkg/util.py
@@ -1,7 +1,7 @@
 """Utility functions."""
 
 
-
+import os
 import re
 import time
 import shutil
@@ -484,20 +484,6 @@ def download_file(url, target_file):
 #    except Exception as ex:
 #        print("Error running shell command: " + str(ex))
         
-
-def run_command(cmd, timeout_seconds=20):
-    try:
-        p = subprocess.run(cmd, timeout=timeout_seconds, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, universal_newlines=True,text=True)
-
-        if p.returncode == 0:
-            return p.stdout # + '\n' + "Command success" #.decode('utf-8')
-            #yield("Command success")
-        else:
-            if p.stderr:
-                return "Error: " + str(p.stderr) # + '\n' + "Command failed"   #.decode('utf-8'))
-
-    except Exception as e:
-        print("Error running command: "  + str(e))
 
 
 def run_command(cmd, timeout_seconds=20):
