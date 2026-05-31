645808ca50861cc9b63596deaae162378b13f8f4
flatsiedatsie <unfold@gmail.com>
2026-05-31 23:59:29 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -227,7 +227,7 @@ cp ./llm/wakeword/open_wake_word/* ./lib/openwakeword/resources/models/
 mkdir -p ./signal
 
 wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 https://media.projektzentrisch.de/temp/signal-cli/signal-cli_ubuntu2004_arm64.gz -O signal-cli_ubuntu2004_arm64.gz
-wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 https://media.projektzentrisch.de/temp/signal-cli/libsignal_jni_so0921_ubuntu2004_arm64.gz -O libsignal_jni_so0921_ubuntu2004_arm64.gz
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 https://media.projektzentrisch.de/temp/signal-cli/libsignal_jni_so0941_ubuntu2004_arm64.gz -O libsignal_jni_so0921_ubuntu2004_arm64.gz
 # Remove local cffi so that the globally installed version doesn't clash
 #rm -rf ./lib/cffi*
 
